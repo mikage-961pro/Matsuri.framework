658718578dd178603bd228e2d89ded28ed6b9378
Apple <apple@MacBook-Pro-AC-Tech-Lead.local>
2020-01-08 00:27:15 +0900

    commit

---
+++ Release/masturi.framework/Headers/masturi.h
@@ -0,0 +1,19 @@
+//
+//  masturi.h
+//  masturi
+//
+//  Created by Apple on 2020/01/08.
+//  Copyright © 2020 Apple lnc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for masturi.
+FOUNDATION_EXPORT double masturiVersionNumber;
+
+//! Project version string for masturi.
+FOUNDATION_EXPORT const unsigned char masturiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <masturi/PublicHeader.h>
+
+
