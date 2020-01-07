34b20354ee3da104a968fa7f865bcefebd657232
Apple <apple@MacBook-Pro-AC-Tech-Lead.local>
2020-01-08 00:16:13 +0900

    first commit

---
+++ masturi/matsuri.swift
@@ -0,0 +1,17 @@
+//
+//  matsuri.swift
+//  masturi
+//
+//  Created by Apple on 2020/01/08.
+//  Copyright © 2020 Apple lnc. All rights reserved.
+//
+
+import Foundation
+
+public class matsuri: NSObject {
+    public func wandaho() {
+           print("matsuri().wandaho is called !!")
+        
+       }
+    
+}
